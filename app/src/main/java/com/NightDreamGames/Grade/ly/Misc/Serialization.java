960e1d4b4a5dbb16af865fb28c459b36d4d2b474
FauconSpartiate <49079695+FauconSpartiate@users.noreply.github.com>
2022-02-02 09:05:00 +0100

    Added error handling for data loading

--- app/src/main/java/com/NightDreamGames/Grade/ly/Misc/Serialization.java
+++ app/src/main/java/com/NightDreamGames/Grade/ly/Misc/Serialization.java
@@ -12,14 +12,24 @@ public class Serialization {
     private static final Gson gson = new Gson();
 
     public static void Serialize() {
-        Manager.writePreference("data", gson.toJson(Manager.years));
-        Manager.writePreference("default_data", gson.toJson(Manager.periodTemplate));
+        try {
+            Manager.writePreference("data", gson.toJson(Manager.years));
+            Manager.writePreference("default_data", gson.toJson(Manager.periodTemplate));
+        } catch (Exception e) {
+            Serialize();
+        }
     }
 
     public static void Deserialize() {
-        Manager.years = gson.fromJson(Manager.getPreference("data", ""), new TypeToken<ArrayList<Year>>() {
-        }.getType());
-        Manager.periodTemplate = gson.fromJson(Manager.getPreference("default_data", ""), new TypeToken<ArrayList<Subject>>() {
-        }.getType());
+        try {
+            Manager.years = gson.fromJson(Manager.getPreference("data", ""), new TypeToken<ArrayList<Year>>() {
+            }.getType());
+            Manager.periodTemplate = gson.fromJson(Manager.getPreference("default_data", ""), new TypeToken<ArrayList<Subject>>() {
+            }.getType());
+        } catch (Exception e) {
+            Manager.deletePreference("data");
+            Manager.deletePreference("default_data");
+            Deserialize();
+        }
     }
 }
